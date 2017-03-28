2ab2582e87f61e25d8a4b0e5efe0d625c7baf3d1
Dalton Morehouse <morehoud5924@gmail.com>
2017-03-28 15:31:24 -0500

    file revision

--- src/dmbannoproject/DMBannoProject.java
+++ src/dmbannoproject/DMBannoProject.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dmbannoproject;
 
 import java.io.IOException;
@@ -15,7 +11,7 @@ import org.apache.http.util.EntityUtils;
 
 /**
  *
- * @author Shadowboom
+ * @author Morehoud
  */
 public class DMBannoProject {
 
@@ -28,21 +24,19 @@ public class DMBannoProject {
         CloseableHttpResponse response1 = httpclient.execute(httpGet);
         BannoFunctions bannoFunctions = new BannoFunctions();
         
-        
-        System.out.println(response1.getStatusLine());
-        HttpEntity entity1 = response1.getEntity();
-        String webpagehtml = EntityUtils.toString(entity1);
-        
-        //System.out.println (webpagehtml);
-        System.out.println("Platform Features Count : " + bannoFunctions.platformFeaturesCount(webpagehtml));
-        System.out.println("Top Alpha Characters : \n" + bannoFunctions.topAlphaChar(webpagehtml));
-        System.out.println("png Count : " + bannoFunctions.pngImagesCount(webpagehtml));
-        System.out.println("Twitter Handle : " + bannoFunctions.bannoTwitterHandle(webpagehtml));
-        System.out.println("Financial Institution Count : " + bannoFunctions.financialinstitutionCount(webpagehtml));
-        EntityUtils.consume(entity1);
-        response1.close();
-        
-        
+        try
+        {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            String webpagehtml = EntityUtils.toString(entity1);
+            System.out.println("Platform Features Count : " + bannoFunctions.platformFeaturesCount(webpagehtml));
+            System.out.println("Top Alpha Characters : \n" + bannoFunctions.topAlphaChar(webpagehtml));
+            System.out.println("png Count : " + bannoFunctions.pngImagesCount(webpagehtml));
+            System.out.println("Twitter Handle : " + bannoFunctions.bannoTwitterHandle(webpagehtml));
+            System.out.println("Financial Institution Count : " + bannoFunctions.financialinstitutionCount(webpagehtml));
+            EntityUtils.consume(entity1);
+        }
+        finally{response1.close();}
     }
     
 }
